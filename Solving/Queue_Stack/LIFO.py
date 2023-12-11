09ea199002768952c2703d0207793d4fb45ab765
bseongwook <qbdmlghkfdyd@gmail.com>
2023-12-11 19:29:43 +0900

    stk

---
+++ Solving/Queue_Stack/LIFO.py
@@ -0,0 +1,23 @@
+# 괄호 유효성 문제
+
+# 소괄호부터 생각 : 1. 짝수일것, 2. 여는게 먼저, ,,,
+# 모든 괄호 포함해서 생각, 마지막에 열린애를 닫기 전에 다른게 열리면 안됨 ( [ ) ] x
+# stack 구조의 맨 위에 있는 놈 확인하고 닫아도 되는지 확인
+
+def isVaild(s):
+    stk = []
+    for p in s:
+        if p == "(":
+            stk.append(")")
+        elif p == "[":
+            stk.append("]")
+        elif p == "{":
+            stk.append("}")
+        elif not stk or stk.pop() != p:
+            return False
+    return not stk    
+        
+
+
+
+
