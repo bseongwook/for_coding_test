30757b815ea57fd3ed4d784661cf6ce5a2289c4e
bseongwook <qbdmlghkfdyd@gmail.com>
2023-12-12 19:04:26 +0900

    0

--- Solving/Hash_table/Dictionary.py
+++ Solving/Hash_table/Dictionary.py
@@ -5,11 +5,31 @@
 
 def TwoSum(nums, target):
     memo = {}
-    for n in nums:
+    for n in nums: # O(n)
         memo[n] = 1
     
     for n in nums: # O(n)
         need_number = target - n
         if need_number in memo: # O(1)
             return True
+        
     return False
+
+print(TwoSum([1,2,3,4], 8))
+
+# 단 위 코드에서는 같은 값을 2번 사용하여 target을 만들 수 있다.
+
+def TwoSum(nums, target):
+    memo = {}
+
+    for n in nums:
+        memo[n] = 1
+
+    for n in nums:
+        need_number = target - n
+        if (need_number in memo) and n != need_number:
+            return True
+        
+    return False
+
+print(TwoSum([1,2,3,4], 8))
