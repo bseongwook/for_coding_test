ce2bdbbcf7f77db78a18172f171c837ee75bbcb3
bseongwook <qbdmlghkfdyd@gmail.com>
2023-12-13 20:12:01 +0900

    0

---
+++ Solving/Tree/Postorder(first).py
@@ -0,0 +1,23 @@
+# lowest common ancestor of a binary tree
+
+# 조상 노드는 문제에 따라 정의 다름
+# 여기서는 나 포함 위의 모든 노드
+
+# 아래의 코드에서 하나의 노드도 작은 트리 취급
+
+# postorder 순회 방식
+def LCA(root, p, q):
+    if root == None:
+        return None
+    
+    left = LCA(root.left, p, q)
+    right = LCA(root.right, p, q)
+
+    if root == p or root == q: # 지나치는 노드가 문제에서 들어온 노드면 해당 노드가 조상
+        return root
+    elif left and right: # 한 노드 입장에서 양쪽의 자식 노드에서 접근하게 된다면 해당 노드가 조상
+        return root
+    
+    return left or right 
+
+LCA([3,5,1,6,2,0,8,None, None,7,4], 6, 4) # 완전 이진트리 형식으로 입력
--- templates/DFS.py
+++ templates/DFS.py
@@ -7,4 +7,29 @@ def dfs(cur_node):
     dfs(cur_node.left)
     dfs(cur_node.right)
 
-dfs(root)
+
+# 접근은 위와 같이, 방문은 언제 하는지에 따라 3가지 방법으로 나눔 (전위, 중위, 후위)
+
+# 전위 순회 preorder
+def preorder(cur_node):
+    if cur_node is None:
+        return
+    print(cur_node.value)
+    preorder(cur_node.left)
+    preorder(cur_node.right)
+
+# 중위 순회 
+def inorder(cur_node):
+    if cur_node is None:
+        return
+    inorder(cur_node.left)
+    print(cur_node.value)
+    inorder(cur_node.right)
+
+# 후위 순회 
+def postorder(cur_node):
+    if cur_node is None:
+        return
+    postorder(cur_node.left)
+    postorder(cur_node.right)
+    print(cur_node.value)
