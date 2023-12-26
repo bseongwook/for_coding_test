e065b397ac90849025951ee6471394f74605a6f8
bseongwook <qbdmlghkfdyd@gmail.com>
2023-12-26 20:52:34 +0900

    0

--- Solved_in_lecture/DP/unique_path.py
+++ Solved_in_lecture/DP/unique_path.py
@@ -8,5 +8,31 @@
 
 
 # 접근방법1 : 완탐 : 격자는 그래프라고 생각
+# 조합 사용해서 오른쪽화살표, 아래화살표 위치 생각하면 풀림
+# 최대는 198C99 = 2 * 10^58
+# 이 문제는 제약조건때문에 못하지만, 제약이 바뀔 수도 있으니 풀어보기
+# 결국 우하단 가려면 우하단 왼쪽, 위쪽 블럭을 지나야됨
+def dfs(r, c):
+    if r == 2 and c == 6:
+        return 1
+    unique_path = 0
+    if r+1 < 3: # 이동 공간 테두리 제한
+        unique_path += dfs(r+1, c)
+    if c+1 < 7:
+        unique_path += dfs(r, c+1)
+    return unique_path
 
+
+# 접근방법2: top down : O(m*n) 수준
+memo = {}
+def dp(r, c):
+    if r == 0 and c == 0:
+        return 1
+    if (r,c) not in memo:
+        if r-1 >= 0:
+            unique_path += dp(r-1, c)
+        if c-1 >= 0:
+            unique_path += dp(r, c-1)
+        memo[(r,c)] = unique_path
+    return memo[(r,c)] # 그냥 갖다 쓰더라도 한 번은 위에서 계산해야 됨
 
