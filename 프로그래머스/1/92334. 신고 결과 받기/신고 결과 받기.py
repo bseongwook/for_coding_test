fe69ae9d9fc4d1c84733cfb5e312afdc3a3f6741
bseongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-07 21:23:41 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -4,7 +4,6 @@ def solution(id_list, report, k):
     report_dic = {i:set() for i in id_list}
     for i in report:
         report_dic[i.split()[0]].add(i.split()[1])    
-    # print(report_dic)
     
     # 사람별로 신고 당한 횟수 저장
     reported_dic = {i:0 for i in id_list}
@@ -12,14 +11,12 @@ def solution(id_list, report, k):
         if len(people) != 0:
             for person in people:
                 reported_dic[person] += 1
-    # print(reported_dic)
     
     # k번 이상 신고받은 사람들 목록 만들기
     lst = []
     for name, num in reported_dic.items():
         if num >= k:
             lst.append(name)
-    # print(lst)
     
     # report_dic과 lst 비교해서, 겹치는 숫자만큼 유저 id 순서로 리스트에 담기
     answer = []
