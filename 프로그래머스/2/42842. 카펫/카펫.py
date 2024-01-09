c1ed241f9c84797e322f9a9d9b2fac643f479aca
bseongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-09 09:28:12 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,11 @@
+
+def solution(brown, yellow):
+    answer = []
+        
+    for y_col in range(1, yellow+1):
+        if yellow % y_col == 0: # 약수라면
+            y_row = yellow // y_col
+        
+        if (y_col+2)*(y_row+2) == brown + yellow:
+            return [y_row+2, y_col+2]
+        
