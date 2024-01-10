333818909db472edcdb882064621a6b571bad138
bseongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-10 16:50:14 +0900

    [level 2] Title: 피로도, Time: 0.15 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -9,11 +9,11 @@ def solution(k_, dungeons):
         k = k_
         cnt = 0 # 해당 case로 탐험을 하면 방문할 수 있는 개수
         for dungeon in each_case:
-            # print(dungeon)
+            
             if dungeon[0] <= k:
                 k -= dungeon[1]
                 cnt += 1
-        # print(cnt)
+
         answer = max(answer, cnt)
         
     return answer
