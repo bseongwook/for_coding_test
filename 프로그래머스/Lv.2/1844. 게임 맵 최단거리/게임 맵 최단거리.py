b73e92ea565251bea3ce1c9dd118d2cd8796f79b
Seongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-16 20:21:11 +0900

    [level Lv.2] Title: 게임 맵 최단거리, Time: 5.37 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/Lv.2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -2,6 +2,8 @@ from collections import deque
 
 def solution(maps):
     
+    # 최단 경로 문제의 경우 방문 노드의 위치와 함께 해당 노드를 몇번째로 방문했는지도 같이 저장하면 좋다
+    
     answer = 0
     
     # if maps[-1][-2] == 0 and maps[-2][-1] == 0: # 도달 못하는 경우
@@ -38,5 +40,7 @@ def solution(maps):
                 q.append((new_x, new_y, answer))
                 
     return -1
+
+
     
     
