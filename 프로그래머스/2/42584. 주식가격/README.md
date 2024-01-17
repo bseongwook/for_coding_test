d4f5566d44d23a0b581f1c83c66f26fac171490a
Seongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-17 20:49:46 +0900

    [level 2] Title: 주식가격, Time: 22.75 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 16.9 MB, 시간: 102.24 ms
+메모리: 16.9 MB, 시간: 22.75 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 3일 20:44:48
+2024년 1월 3일 20:49:44
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,19 +1,32 @@
-def solution(prices):
-    answer = []
+# def solution(prices):
+#     answer = []
     
-    for idx in range(len(prices)-1): # 0~7
+#     for idx in range(len(prices)-1): # 0~7
         
-        i = idx+1
+#         i = idx+1
         
-        while i < len(prices):
-            if prices[idx] > prices[i]: # 떨어진 경우
-                answer.append(i-idx)
-                break
-            else:
-                i += 1
+#         while i < len(prices):
+#             if prices[idx] > prices[i]: # 떨어진 경우
+#                 answer.append(i-idx)
+#                 break
+#             else:
+#                 i += 1
         
-        if i == len(prices): # 끝까지 안떨어진 경우
-            answer.append(len(prices) - idx - 1)
+#         if i == len(prices): # 끝까지 안떨어진 경우
+#             answer.append(len(prices) - idx - 1)
         
-    answer.append(0)
+#     answer.append(0)
+#     return answer
+
+def solution(prices):
+    stack = []
+    answer = [0] * len(prices)
+    for i in range(len(prices)):
+        if stack != []:
+            while stack != [] and stack[-1][1] > prices[i]:
+                past, _ = stack.pop()
+                answer[past] = i - past
+        stack.append([i, prices[i]])
+    for i, s in stack:
+        answer[i] = len(prices) - 1 - i
     return answer
