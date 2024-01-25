63066338c8b80a6bc23affbb83fe2fdef70755d9
Seongwook <150785682+bseongwook@users.noreply.github.com>
2024-01-25 14:25:24 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 3.06 ms, Memory: 10.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -0,0 +1,26 @@
+def solution(files):
+    answer = []
+    
+    for file in files:
+        HEAD = ''
+        NUMBER = ''
+        TAIL = ''
+        done_head = False
+        done_number = False
+        # 숫자가 6개인 경우도 고려
+        for f in file:
+            if not done_head and not f.isdigit():
+                HEAD += f
+            elif not done_number and f.isdigit():
+                NUMBER += f
+                done_head = True
+            else:
+                done_number = True
+                TAIL += f
+        answer.append((HEAD, NUMBER, TAIL))
+        
+    answer.sort(key = lambda x: (x[0].lower(), int(x[1]) ))
+    lst = []
+    for i in answer:
+        lst.append("".join(i))
+    return lst
